45670abf3cb5bbb11f4551f2b1cb96c02b81c753
smnra <hppall@163.com>
2017-10-16 17:08:43 +0800

    0.3

--- ssh.py
+++ ssh.py
@@ -1,6 +1,9 @@
 #coding=utf-8
 
 import paramiko
+from stat import S_ISDIR
+import os
+
 
 hostname = '192.168.192.130'
 username = 'smnra'
@@ -25,29 +28,65 @@ class RemoteManage():
         except Exception as e :
             print(str(e))
 
-    def getfile(self, localpath, remotepath):
+    def getFile(self, remotefile, localfile):
         try:
             self.sftp = self.ssh.open_sftp()
-            print(self.sftp.listdir('/'))    #打印目录列表
-            self.sftp.get(remotepath, localpath)    #下载文件
+            self.sftp.get(remotefile, localfile)    #下载文件
         except Exception as e :
             print(str(e))
 
-    def putfile(self, localpath, remotepath):
+    def putFile(self, localfile, remotefile):
         try:
             self.sftp = self.ssh.open_sftp()
-            print(self.sftp.listdir('/'))    #打印目录列表
-            self.sftp.put(localpath, remotepath)    #下载文件
+            #print(self.sftp.listdir('/'))    #打印目录列表
+            self.sftp.put(localfile, remotefile)    #下载文件
         except Exception as e :
             print(str(e))
 
+
+    # ------获取远端linux主机上指定目录及其子目录下的所有文件------
+    def getRemoteDir(self, remote_dir):
+        # 保存所有文件的列表
+        all_files = list()
+
+        # 去掉路径字符串最后的字符'/'，如果有的话
+        if remote_dir[-1] == '/':
+            remote_dir = remote_dir[0:-1]
+
+        # 获取当前指定目录下的所有目录及文件，包含属性值
+
+        try:
+            self.sftp = self.ssh.open_sftp()
+            #print(self.sftp.listdir('/'))    #打印目录列表
+            files = self.sftp.listdir_attr(remote_dir)   #listdir_attr 获取远程主机路径上的文件列表包含文件属性
+            for x in files:
+                # remote_dir目录中每一个文件或目录的完整路径
+                filename = remote_dir + '/' + x.filename
+                # 如果是目录，则递归处理该目录，这里用到了stat库中的S_ISDIR方法，与linux中的宏的名字完全一致
+                if S_ISDIR(x.st_mode):
+                    all_files.extend(self.getRemoteDir(filename))
+                else:
+                    all_files.append(filename)
+            return all_files
+        except Exception as e :
+            print(str(e))
+
+    def getDir(self, remotepath, localpath):
+            # 获取远端linux主机上指定目录及其子目录下的所有文件
+            all_files = self.getRemoteDir(remotepath)
+            # 依次get每一个文件
+            for x in all_files:
+                filename = x.split('/')[-1]
+                local_filename = os.path.join(localpath, filename)
+                print(u'Get文件%s传输中...' % filename)
+                self.sftp.get(x, local_filename)
 
 if __name__ == '__main__' :
     remote = RemoteManage(hostname, username, password, port = 22, cmd = 'ls -l')
     result = remote.exec('ls -l /')
     print(result)
-    remote.getfile('/home/smnra/getip/requirements.txt', 'requirements.txt')
-
+    #remote.getFile('/home/smnra/getip/requirements.txt', 'requirements.txt')
+    print(remote.getRemoteDir('/etc/acpi'))
     remote.ssh.close() #关闭连接
 
 
