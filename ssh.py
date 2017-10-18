5ac71d43135f9400794184d5e9a8972d682d5f39
smnra <hppall@163.com>
2017-10-18 14:50:29 +0800

    0.4

--- ssh.py
+++ ssh.py
@@ -81,12 +81,29 @@ class RemoteManage():
                 print(u'Get文件%s传输中...' % filename)
                 self.sftp.get(x, local_filename)
 
+    def getPathDir(self, remotepath, localpath):
+            # 获取远端linux主机上指定目录及其子目录下的所有文件
+            all_files = self.getRemoteDir(remotepath)
+            # 依次get每一个文件
+            for x in all_files:
+                filename = x.split('/')[-1]             #取列表最后一个元素
+                otherpath = x.replace(remotepath + "/","").replace(filename,"")      #把远程完整文件名  中包含的远程路径 删除,并且把文件名删除
+                if len(otherpath) > 0 :     #如果远程存在文件夹,
+                    local_dir = localpath  + otherpath      #拼接本地文件夹路径
+                    if  not os.path.exists(local_dir) :
+                        os.makedirs(local_dir)       #在本文件夹建立远程文件夹
+                    local_filename = os.path.join(local_dir, filename)     #本地完整文件名
+                else :
+                    local_filename = os.path.join(localpath, filename)
+                print(u'Get文件%s传输中...' % local_filename)
+                self.sftp.get(x, local_filename)
+
 if __name__ == '__main__' :
     remote = RemoteManage(hostname, username, password, port = 22, cmd = 'ls -l')
     result = remote.exec('ls -l /')
     print(result)
     #remote.getFile('/home/smnra/getip/requirements.txt', 'requirements.txt')
-    print(remote.getRemoteDir('/etc/acpi'))
+    remote.getPathDir('/etc/acpi',"./")
     remote.ssh.close() #关闭连接
 
 
